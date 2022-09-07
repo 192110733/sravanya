18b36c228060680380160524b1d7ab6ea4783e24
192110733 <112925366+192110733@users.noreply.github.com>
2022-09-07 14:12:25 +0530

    Add files via upload

---
+++ uppercase.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i;
+	int upper=0,lower=0,num=0,special=0;
+	printf("please enter the string\n");
+	gets(str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>'A'&&str[i]<='Z')
+		{
+			upper++;
+		}
+		else if(str[i]>='a'&&str[i]<='z')
+		{
+			lower++;
+		}
+		else if(str[i]='1'&&str[i]<=9)
+		{
+			num++;
+		}
+		else
+		{
+			special++;
+		}
+	}
+	printf("\nUpper case letter:%d",upper);
+	printf("\nlowerr case letter:%d",lower);
+	printf("\nNumber:%d",num);
+	printf("\nSpecial characters:%d",special);
+	return 0;
+}
